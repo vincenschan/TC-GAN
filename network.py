772c58fd207315bc07ceea03ac298a5b1fa12718
肖奋溪 <xiaofenxi@xiaofenxideMacBook-Pro.local>
2019-03-14 16:19:36 +0800

    base framework

--- network.py
+++ network.py
@@ -11,7 +11,7 @@ from tensorlayer.layers import (
 )
 
 class GAN(object):
-    def __init__(self, class_num, images_size, channel=3, code_dim=128, is_train=True, test_mode="generator"):
+    def __init__(self, class_num, images_size, channel=3, code_dim=128, Lcategory_penalty=1.0, is_train=True, test_mode="generator"):
         # fake picture class
         self.class_num = class_num
         # input && output image size n x n
@@ -20,6 +20,7 @@ class GAN(object):
         self.channel = channel
         # code dimension for noise to input 
         self.code_dim = code_dim
+        self.Lcategory_penalty = Lcategory_penalty
         # placeholder for noise input
         self.z = tf.placeholder(tf.float32, [None, code_dim], name='z_noise')
         self.label_class = tf.placeholder(tf.int32, [None, ], name='label_class')
@@ -103,26 +104,53 @@ class GAN(object):
             # category loss
             net_h4_2 = DenseLayer(net_h4, n_units=self.class_num, act=tf.identity,
                     W_init = w_init, name='d/h4/lin_sigmoid')
-            logits = net_h4.outputs
+            net_h4_1_logits = net_h4_1.outputs
+            net_h4_2_logits = net_h4_2.outputs
             net_h4_1.outputs = tf.nn.sigmoid(net_h4_1.outputs)
-        return net_h4_1, logits, net_h4_2
+            net_h4_2.outputs = tf.nn.sigmoid(net_h4_2.outputs)
+        return net_h4_1, net_h4_1_logits, net_h4_2, net_h4_2_logits
     
     def _build_network(self):
         # Input noise into generator for training
         self.net_g = self.generator(self.z, self.label_class, is_train=True, reuse=False)
         # Input real and generated fake images into discriminator for training
-        self.net_d_1, self.d_logits, self.net_d_2 = self.discriminator(self.net_g.outputs, is_train=True, reuse=False)
-        _, self.d2_logits, self.net_d2_2 = self.discriminator(self.real_images, is_train=True, reuse=True)
+        self.net_d_1, self.d_1_logits, self.net_d_2, self.d_2_logits = self.discriminator(self.net_g.outputs, is_train=True, reuse=False)
+        _, self.d2_1_logits, self.net_d2_2, self.d2_2_logits = self.discriminator(self.real_images, is_train=True, reuse=True)
         # Input noise into generator for evaluation
         # set is_train to False so that BatchNormLayer behave differently
         self.net_g2 = self.generator(self.z, self.label_class, is_train=False, reuse=True)
 
         # define loss
         # discriminator: real images are labelled as 1
-        self.d_loss_real = tl.cost.sigmoid_cross_entropy(self.d2_logits, tf.ones_like(self.d2_logits), name='dreal')
-
+        self.d_loss_real = tl.cost.sigmoid_cross_entropy(self.d2_1_logits, tf.ones_like(self.d2_1_logits), name='d_loss_real')
         # discriminator: images from generator (fake) are labelled as 0
-        self.d_loss_fake = tl.cost.sigmoid_cross_entropy(self.d_logits, tf.zeros_like(self.d_logits), name='dfake')
+        self.d_loss_fake = tl.cost.sigmoid_cross_entropy(self.d_1_logits, tf.zeros_like(self.d_1_logits), name='d_loss_fake')
+        # generator:images from generator (fake) and labelled as 1
+        self.g_cheat_loss = tl.cost.sigmoid_cross_entropy(self.d_1_logits, tf.ones_like(self.d_1_logits), name='g_cheat_loss')
+        # classify the image from real image
+        self.category_loss_real = tl.cost.sigmoid_cross_entropy(self.d2_2_logits, tf.one_hot(indices=self.label_class, depth=self.class_num), name='category_loss_real')
+        # classify the image from fake image
+        self.category_loss_fake = tl.cost.sigmoid_cross_entropy(self.d_2_logits, tf.one_hot(indices=self.label_class, depth=self.class_num), name='category_loss_fake')
+        
+        # total loss
+        self.category_loss = self.Lcategory_penalty * (self.category_loss_real + self.category_loss_fake)
+        self.d_loss = self.d_loss_real + self.d_loss_fake + self.category_loss / 2.0
+        self.g_loss = self.g_cheat_loss + self.category_loss_fake * self.Lcategory_penalty
+
+        # set summary (record in tensor board)
+        
+        self.d_loss_real_summary = tf.summary.scalar("d_loss_real", self.d_loss_real)
+        self.d_loss_fake_summary = tf.summary.scalar("d_loss_fake", self.d_loss_fake)
+        self.g_cheat_loss_summary  = tf.summary.scalar("g_cheat_loss", self.g_cheat_loss)
+        self.category_loss_real_summary  = tf.summary.scalar("category_loss_real", self.category_loss_real)
+        self.category_loss_fake_summary  = tf.summary.scalar("category_loss_fake", self.category_loss_fake)
+        self.category_loss_summary = tf.summary.scalar("category_loss", self.category_loss)
+        self.d_loss_summary = tf.summary.scalar("d_loss", self.d_loss)
+        self.g_loss_summary = tf.summary.scalar("g_loss", self.g_loss)
+
+        self.d_merged_summary = tf.summary.merge([self.d_loss_real_summary, self.d_loss_fake_summary,
+                                             self.category_loss_summary, self.d_loss_summary])
+        self.g_merged_summary = tf.summary.merge([self.g_cheat_loss_summary, self.category_loss_fake_summary])
 
     def _build_generator(self):
         pass
